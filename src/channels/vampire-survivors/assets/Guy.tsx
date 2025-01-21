67cc3c6b075a84df4fa25600b5df61a1d761e0a0
ryanttb <ryan@trippingthebits.com>
2025-01-21 17:30:15 -0500

    switch to tabs

--- src/channels/vampire-survivors/assets/Guy.tsx
+++ src/channels/vampire-survivors/assets/Guy.tsx
@@ -3,44 +3,44 @@ import guySprite from './guy.png';
 import { useEffect, useState } from 'react';
 
 interface GuyContainerProps {
-    sprite: string;
-    frameX: number;
-    left?: string;
-    top?: string;
+	sprite: string;
+	frameX: number;
+	left?: string;
+	top?: string;
 }
 
 const GuyContainer = styled.div<GuyContainerProps>`
-    position: absolute;
-    left: ${props => props.left || '50%'};
-    top: ${props => props.top || '35%'};
-    width: 101px;
-    height: 104px;
-    background-image: url(${props => props.sprite});
-    background-repeat: no-repeat;
-    overflow: hidden;
-    background-position: ${props => `-${props.frameX * 101}px 0`};
+	position: absolute;
+	left: ${props => props.left || '50%'};
+	top: ${props => props.top || '35%'};
+	width: 101px;
+	height: 104px;
+	background-image: url(${props => props.sprite});
+	background-repeat: no-repeat;
+	overflow: hidden;
+	background-position: ${props => `-${props.frameX * 101}px 0`};
 `;
 
 interface GuyProps {
-    left?: string;
-    top?: string;
+	left?: string;
+	top?: string;
 }
 
 
 function Guy({left, top}: GuyProps) {
-    const [frame, setFrame] = useState(0);
+	const [frame, setFrame] = useState(0);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setFrame(prevFrame => (prevFrame + 1) % 2);
-        }, 1000 / 2);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setFrame(prevFrame => (prevFrame + 1) % 2);
+		}, 1000 / 2);
 
-        return () => clearInterval(interval);
-    }, []);
+		return () => clearInterval(interval);
+	}, []);
 
-    return (
-        <GuyContainer sprite={guySprite} frameX={frame} left={left} top={top} />
-    );
+	return (
+		<GuyContainer sprite={guySprite} frameX={frame} left={left} top={top} />
+	);
 }
 
 export default Guy;
